585e56f2e95032753c9b1f44994a97b0dbfe8df4
teanadros <teanadros@gmail.com>
2026-08-21 11:30:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tension-v1";
+const CACHE = "tension-v3";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
